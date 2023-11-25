90937cc1097e579d6cab65bc7b654603fbffc136
VICTOR-CHUKWU <okenwa1993@gmail.com>
2023-11-25 21:18:03 +0100

    fixes

--- src/data/about.ts
+++ src/data/about.ts
@@ -1,24 +1,24 @@
 export const teams = [
     {
-        img: '/img/teams/team1.jpg',
+        img: '/img/teams/team1.webp',
         name: 'Aransiola Tolu Olaitan',
         role: 'Managing Director',
         description: `text needed`
     },
     {
-        img: '/img/teams/team3.jpg',
+        img: '/img/teams/team3.webp',
         name: 'Innocent Ebunada',
         role: 'General Manager',
         description: `text needed`
     },
     {
-        img: '/img/teams/team4.jpg',
+        img: '/img/teams/team4.webp',
         name: 'Adeyanju Seun Stephen',
         role: 'Regional Manager, South West',
         description: `text needed`
     },
     {
-        img: '/img/teams/team2.jpg',
+        img: '/img/teams/team2.webp',
         name: 'Faith Gilbert',
         role: 'Clients service relations',
         description: `text needed`
